21f4d812d2151ebedfc877aa07fc4020036f0f52
Mohammad Zaid <zaidcoder@gmail.com>
2024-04-16 13:32:39 +0530

    updated Controller

--- backend/entitymanager-service/Controller/UserController.py
+++ backend/entitymanager-service/Controller/UserController.py
@@ -12,36 +12,35 @@ app = Flask(__name__)
 @app.route('/register/driver', methods=['POST'])
 def register_driver():
     driver = request.get_json()
-    driver_id = UserDAO.UserDAO.create_driver(driver['data'])
+    driver_id = UserDAO.UserDAO.create_driver(driver['data'].name, driver['data'].password, driver['data'].email, driver['data'].phone, driver['data'].driving_license)
     return jsonify({'driver_id' : driver_id})
 
 @app.route('/register/passenger', methods=['POST'])
 def register_passenger():
     passenger = request.get_json()
-    passenger_id = UserDAO.UserDAO.create_passenger(passenger['data'])
+    passenger_id = UserDAO.UserDAO.create_passenger(passenger['data'].name, passenger['data'].password, passenger['data'].email, passenger['data'].phone)
     return jsonify({'passenger_id' : passenger_id})
 
 @app.route('/driver/add_vehicle', methods=['POST'])
 def add_vehicle():
     driver_id = request.get_json()['driver_id']
     vehicle = request.get_json()['vehicle']
-    UserDAO.UserDAO.add_vehicle_to_driver(driver_id, vehicle)
-    return jsonify({'status' : 'success'})
+    vehicle_id = UserDAO.UserDAO.add_vehicle(driver_id, vehicle['vehicle_model'], vehicle['registration_number'], vehicle['insurance_number'], vehicle['manufacturer'], vehicle['manufacturing_year'])
+    return jsonify({'status' : vehicle_id})
 
-@app.route('/login/user', methods=['POST'])
-def login_user():
-    user = request.get_json()
-    user_id = UserDAO.UserDAO.login_user(user['data'])
-    jwt_token = jwt.encode({'user_id' : user_id}, 'secret', algorithm='HS256')
-    return jsonify({'token' : jwt_token})
+@app.route('/login/passenger', methods=['POST'])
+def login_passenger():
+    passenger = request.get_json()
+    passenger_id = UserDAO.UserDAO.login_passenger(passenger['data'].phone, passenger['data'].password)
+    jwt_token = jwt.encode({'passenger_id' : passenger_id}, 'secret', algorithm='HS256')
+    return jsonify({'passenger_id':passenger_id, 'token' : jwt_token})
 
 @app.route('/login/driver', methods=['POST'])
 def login_driver():
     driver = request.get_json()
-    driver_id = UserDAO.UserDAO.login_driver(driver['data'])
+    driver_id = UserDAO.UserDAO.login_driver(driver['data'].phone,driver['data'].password)
     jwt_token = jwt.encode({'driver_id' : driver_id}, 'secret', algorithm='HS256')
-    return jsonify({'driver_id' : jwt_token})
+    return jsonify({'driver_id' : driver_id,'token':jwt_token})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
--- backend/entitymanager-service/app.py
+++
@@ -1,9 +0,0 @@
-# app.py (Entry point)
-from flask import Flask
-from Controller.UserController import user_blueprint
-
-app = Flask(__name__)
-app.register_blueprint(user_blueprint, url_prefix='/api/users')
-
-if __name__ == '__main__':
-    app.run(debug=True)
