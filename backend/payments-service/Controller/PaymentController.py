d41d8aa595f193fd6f0ddff1dc4126f0dce7dd39
Shambhavi@28 <shambhavi.jahagirdar@research.iiit.ac.in>
2024-04-17 19:11:05 +0530

    payment interface

--- backend/payments-service/Controller/PaymentController.py
+++ backend/payments-service/Controller/PaymentController.py
@@ -1,6 +1,9 @@
 
 
 from interfaces import PaymentInterface
+from services import CreditCardPayment, DebitCardPayment, UPIPayment
+from Entities import Payment, Wallet
+from DAO import PaymentsDAO
 
 
 ### Crux of the payment microservice
@@ -12,4 +15,30 @@ from interfaces import PaymentInterface
 ### One route fors each payment strategy
 ### input taken by controller -> ride_id , amount , user_id , payment method and any other attribute if needed
 class PaymentController : 
-    pass 
+
+    def __init__(self, ride_id, amount, user_id, payment_method, date, driver_id) :
+        self.payment_details = Payment(amount, payment_method, date, ride_id, user_id, driver_id)
+        # payment_method_instance should have CreditCardPayment() etc
+        # self.payment_method = self.set_payment_method(payment_method)
+
+    # for payment to be made:
+    # different services
+    # accessing and modifying database objects
+
+    def set_payment_method(self, payment_method, details_in_tuple) :
+        
+        if payment_method == 'CreditCard':
+            return CreditCardPayment(*details_in_tuple)
+        
+        elif payment_method == 'DebitCard':
+            return DebitCardPayment(*details_in_tuple)
+        
+        elif payment_method == 'UPI':
+            return UPIPayment(*details_in_tuple)
+    
+
+    def execute(self) :
+        self.payment_method.pay(self.amount)
+        success = PaymentsDAO.add_payment_record(self.payment_details)
+        if(success):
+            self.payment_details.set_status("completed")
--- backend/payments-service/Entities/Payment.py
+++ backend/payments-service/Entities/Payment.py
@@ -1,1 +1,20 @@
 '''This will store the payment object that will be saved in the Database.'''
+
+class Payment:
+
+    def __init__(self, amount, payment_method, date, ride_id, user_id, driver_id):
+        
+        self.id = self.generate_id()
+        self.ride_id = ride_id
+        self.user_id = user_id
+        self.driver_id = driver_id
+        self.amount = amount
+        self.payment_method = payment_method
+        self.date = date
+        self.__status = "pending"
+
+    def set_status(self, status):
+        self.__status = status
+
+    def generate_id(self):
+        self.id = self.user_id + self.ride_id
